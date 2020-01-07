9db12a88f75e0b68c81e748e9005eb7c82ed8755
Musaab Yousif <myn8219@wmich.edu>
2020-01-07 16:27:55 -0500

    Clock, array set up

--- a1/a1.c
+++ a1/a1.c
@@ -9,9 +9,31 @@ Assignment #1   //assignment title
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <apue.h>
+#include <array.h>
+#include <linked.h>
 
 
-double result = 0;
+
+int main(int argc, char *argv[]){
+
+int size = NULL;
+double result;
 
 printf("\nrand %d", rand() % 2000 + 10000);
 result = (double) rand() / RAND_MAX;
+size = result;
+}
+
+double * createArray(int * size){
+
+int i;
+size = (double) (rand() % 2000) + 10000;
+
+int *array = malloc(size * sizeof(*array));
+
+for(i = 0; i < size; i++){
+    array[i] = (double) rand() / RAND_MAX;
+}
+    return array, size;
+}
--- a1/array.c
+++ a1/array.c
@@ -1,3 +1,23 @@
-int array(double * array, int size);  // returns 0 ok, -1 on error; return to main do not exit in this function
+#include <time.h>
+#include <stdio.h>
+
+
+int array(double * array, int size){
+
+    clock_t start, end;
+    double timeUsed;
+    start = clock();
+
+    printf("In array.\nSize: %i", size);
+
+    
+
+end = clock();
+timeUsed = (end - start) / CLOCKS_PER_SEC;
+printf("Sort done.\n Time: %d", timeUsed);
+
+};  // returns 0 ok, -1 on error; return to main do not exit in this function
+
 
 double * createArray(int *size);
+
